c5e40ccbd0490b4137991ef8820c41a61fdff1b7
Diya050 <124448340+Diya050@users.noreply.github.com>
2023-02-24 20:35:25 +0530

    Update notes.js

--- notes.js
+++ notes.js
@@ -81,12 +81,11 @@ reposEl.appendChild(repoEl)
 }
 
 
-form.addEventListener('submit', call())
-function call(){
+form.addEventListener('submit', (e) => {
 e.preventDefault()
 const user = search.value
 if(user) {
 getUser(user)
 search.value = ''
 }
-}
+})
